2c0f0df79a8c89ceffd83d16d6357f1e06af53a3
Emmsii <matty.93@hotmail.com>
2014-07-11 13:54:37 +0100

    - Implemented loading custom rooms using json-simple library.

--- DungeonCrawler/src/com/egs/dungeon/util/FileHandler.java
+++ DungeonCrawler/src/com/egs/dungeon/util/FileHandler.java
@@ -1,12 +1,13 @@
 package com.egs.dungeon.util;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -54,22 +55,103 @@ public class FileHandler {
 		return new File("res/rooms/").listFiles().length;
 	}
 	
-	public String[] loadRoom(String name){
-		file = new File("res/rooms/" + name + ".txt");
+	public int[][] loadRoomTileData(String name){
+		file = new File("res/rooms/" + name + ".json");
 		if(!file.exists()) return null;
+
+		int[] tileData;
+		int[][] result;
 		
-		List<String> lines = new ArrayList<String>();
-		String line = null;
 		try{
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			while((line = reader.readLine()) != null){
-				lines.add(line);
+			FileReader reader = new FileReader(file);
+			JSONParser jsonParser = new JSONParser();
+			JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+			
+			long width = (long) jsonObject.get("width");
+			long height = (long) jsonObject.get("height");
+			JSONArray data = (JSONArray) jsonObject.get("tileData");
+
+			tileData = new int[(int) (width * height)];
+			result = new int[(int) width][(int) height];
+			
+			for(int i = 0; i < data.size(); i++){
+				String raw = data.get(i).toString();
+				tileData[i] = Integer.parseInt(raw);
 			}
-			reader.close();
-			return lines.toArray(new String[lines.size()]);
+			
+			for(int y = 0; y < height; y++){
+				for(int x = 0; x < width; x++){
+					result[x][y] = tileData[(int) (x + y * width)];
+				}
+			}
+			
 		}catch(IOException e){
 			e.printStackTrace();
 			return null;
+		}catch(ParseException e){
+			e.printStackTrace();
+			return null;
 		}
+		
+		return result;
 	}
+	
+	public int[][] loadConnectData(String name){
+		file = new File("res/rooms/" + name +".json");
+		if(!file.exists()) return null;
+		
+		int[] connectData;
+		int[][] result;
+		
+		try{
+			FileReader reader = new FileReader(file);
+			JSONParser jsonParser = new JSONParser();
+			JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+			
+			long width = (long) jsonObject.get("width");
+			long height = (long) jsonObject.get("height");
+			JSONArray data = (JSONArray) jsonObject.get("connectData");
+			
+			connectData = new int[(int) (width * height)];
+			result = new int[(int) width][(int) height];
+			
+			for(int i = 0; i < data.size(); i++){
+				String raw = data.get(i).toString();
+				connectData[i] = Integer.parseInt(raw);
+			}
+			
+			for(int y = 0; y < height; y++){
+				for(int x = 0; x < width; x++){
+					result[x][y] = connectData[(int) (x + y * width)];
+				}
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+			return null;
+		}catch(ParseException e){
+			e.printStackTrace();
+			return null;
+		}
+		return result;
+	}
+	
+	
+//	public String[] loadRoom(String name){
+//		file = new File("res/rooms/" + name + ".txt");
+//		if(!file.exists()) return null;
+//		
+//		List<String> lines = new ArrayList<String>();
+//		String line = null;
+//		try{
+//			BufferedReader reader = new BufferedReader(new FileReader(file));
+//			while((line = reader.readLine()) != null){
+//				lines.add(line);
+//			}
+//			reader.close();
+//			return lines.toArray(new String[lines.size()]);
+//		}catch(IOException e){
+//			e.printStackTrace();
+//			return null;
+//		}
+//	}
 }
